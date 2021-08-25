1908050d8b49a618542005bc3cf965af02bb8793
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-25 14:10:32 -0500

    [setup_web_static] Change aliasses

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,6 +12,6 @@ ln -s /data/web_static/releases/test/ /data/web_static/current
 chown ubuntu /data/
 chown :ubuntu /data/
 found="server_name _;"
-adding="\n\tlocation /static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}"
+adding="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}"
 sed -i "/$found/a\\$adding" /etc/nginx/sites-available/default
 service nginx restart
