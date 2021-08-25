c9c51e262f51d4174e39348a69422c245a6025c7
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-25 15:27:14 -0500

    [setyp_web_static] Fix all !

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,8 +9,8 @@ echo '
 	<body><h1>Fake body</h1></body>
 </html>' > /data/web_static/releases/test/index.html
 ln -s /data/web_static/releases/test/ /data/web_static/current
-chown ubuntu /data/
-chown :ubuntu /data/
+chown -R ubuntu /data/
+chown -R :ubuntu /data/
 found="server_name _;"
 adding="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}"
 sed -i "/$found/a\\$adding" /etc/nginx/sites-available/default
