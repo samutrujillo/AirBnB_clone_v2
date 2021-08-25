32369b6d16c1157acc2dc5ea57a221da26847b63
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-25 13:40:41 -0500

    [0-setup_web_static] Done!

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,4 +15,3 @@ found="server_name _;"
 adding="\n\tlocation /static/ {\n\t\talias /data/web_static/current/\n\t\tautoindex off;\n\t}"
 sed -i "/$found/a\\$adding" /etc/nginx/sites-available/default
 service nginx restart
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpJUvI3G5oiVtuzRkZQxfZlO3GV1Xy0yegt8hJQ1mhHwLvhSrXzZucU259GQSs6PI5AchohvmQv/xg9saFZWgi42WcWN0gS4rs1oHUhqVxpWx0hPmY8A0ZnjQ+kfOCpVBfAFTQVaawUFP5B49L84WSmbLBHJGlvzPMDhi31VKlKmquTXQ/y2jxcB6B/i6tflf2384579+8LZ3dwPeWDSFzNyvy/07WknQ5OsKs//d2ih3ooGSs7rCzyZVMQ6s4nJUF1azxH2H15H02OIejmj2Ouja1PwDYgtmY9tsb6Rv6GANe+ZwG9u5hl22+SuH06WNR07nxP79/zF4Z2I33q9yb manuel@debian
