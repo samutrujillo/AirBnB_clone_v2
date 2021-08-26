3971054a021760981fe9dc2b6d7e754f7bb92637
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-26 12:05:43 -0500

    [pack_web_static] Remove prints!

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,6 @@
 """
 from fabric.api import local
 from datetime import datetime
-import os, stat
 
 
 def do_pack():
@@ -13,11 +12,9 @@ def do_pack():
                                                     datetime.now().hour,
                                                     datetime.now().minute,
                                                     datetime.now().second)
-    if not os.path.isdir('versions'):
-        os.mkdir('versions')
-    print("Packing web_static to versions/{}".format(compress))
-    local("tar -cvzf versions/{} web_static".format(compress))
-    size = os.path.getsize("versions/{}".format(compress))
-    print("web_static packed: versions/{} -> {}Bytes".format(compress, size))
-    os.chmod("versions/{}".format(compress), 436)
-    return "versions/{}".format(compress)
+    local("mkdir versions/")
+    try:
+        local("tar -cvzf versions/{} web_static".format(compress))
+        return "versions/{}".format(compress)
+    except:
+        return None
