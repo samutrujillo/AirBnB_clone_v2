ddea56f1221f98c477885d01e23b007ec4bee857
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-26 12:20:15 -0500

    [setup_web_static] update!

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,17 @@
 # Script to configure servers
 apt-get update && apt-get -y install nginx
 mkdir -p /data/web_static/releases/test
-mkdir /data/web_static/shared
+mkdir -p /data/web_static/shared
 echo '
 <html>
 	<head><title>Hello</title></head>
 	<body><h1>Fake body</h1></body>
 </html>' > /data/web_static/releases/test/index.html
-ln -s /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chown -R :ubuntu /data/
 found="server_name _;"
 adding="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}"
 sed -i "/$found/a\\$adding" /etc/nginx/sites-available/default
 service nginx restart
+exit 0
