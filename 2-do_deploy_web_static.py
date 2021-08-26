329b8a9a95e44ef39b8f7b1485b18f4b72d66297
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-08-26 13:55:00 -0500

    add task 2 and delete task 0

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,41 @@
 #!/usr/bin/python3
-"""Module to compress files
 """
+script that distributes archive to webservers
+"""
+import os.path
 from fabric.api import *
-from datetime import datetime
-import os
+from fabric.operations import run, put, sudo
 
+env.hosts = ['34.73.75.113', '54.163.209.225']
 
-
-env.hosts = ["ubuntu@34.73.252.190", "ubuntu@54.198.140.74"]
-
-def do_pack():
-    """This method is to compress all files
-    """
-    date_time = datetime.now()
-    compress = "web_static_{}.tgz".format(date_time.strftime("%Y%m%d%H%M%S"))
-    local("mkdir -p versions/")
-    try:
-        comp_file = local("tar -cvzf versions/{} web_static/".format(compress))
-        return "versions/{}".format(compress)
-    except:
-        return None
 
 def do_deploy(archive_path):
-    """Method to deploy files
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    if not os.path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
 
-    else:
-        try:
-            name = archive_path.split("/")[1][:-4]
-            put(archive_path, '/tmp')
-            run("mkdir -p /data/web_static/releases/{}".format(name))
-            command = "tar -xzf /tmp/{}.tgz -C ".format(name)
-            command += "/data/web_static/releases/{}".format(name)
-            run(command)
-            run("rm /tmp/{}.tgz".format(name))
-            command = "mv /data/web_static/releases/{}/".format(name)
-            command += "web_static/* /data/web_static/releases/{}/".format(name)
-            run(command)
-            run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-            run("rm -rf /data/web_static/current")
-            command = "ln -s /data/web_static/releases/{}/ ".format(name)
-            command += "/data/web_static/current"
-            run(command)
-            return True
-        except Exception as err:
-            print(err)
-            return False
+    try:
+        file = archive_path.split('/')[-1]
+        new_folder = ("/data/web_static/releases/" + file.split(".")[0])
+        # Upload the archive to the /tmp/
+        put(archive_path, "/tmp/")
+        # Uncompress the archive to the folder /data/web_static/releases/
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".format(file, new_folder))
+        # Delete the archive from the web server
+        run("sudo rm /tmp/{}".format(file))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        # Delete the symbolic link
+        run('sudo rm -rf /data/web_static/current')
+        # Create a new the symbolic link
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        return True
+    except:
+        return False
