0b6357e6327b75f3ef2077dca82082dc8da429d6
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-08-26 13:40:08 -0500

    add task 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# script
+
+# install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
+
+#create folders
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+
+# Create a fake HTML file
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+#simbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#ownership and group
+sudo chown -R ubuntu:ubuntu /data
+
+#routing to web static
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+
+#restart nginx
+sudo service nginx restart
